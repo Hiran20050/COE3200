a7548d7e808aac3fedfd8676f808e0ab2696e0bc
Hiran20050 <s25007990@ousl.lk>
2026-01-15 19:11:32 +0530

    Add files via upload

---
+++ # hello.py
@@ -0,0 +1,9 @@
+# hello.py
+# A simple script to print a greeting massage
+
+def main():
+    print("Hello! I  am Hiran Erandaka(Hiran20050).")
+    print("Welcome to my Assigment Repository.")
+
+if __name__ == "main":
+    main()
